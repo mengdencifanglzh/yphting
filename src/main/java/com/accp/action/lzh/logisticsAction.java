4f27113cc29eed5089046964ae1121f8d89104d9
java17lzh <574003068@qq.com>
2018-10-19 23:08:37 +0800

    发物流完成10.19--23：08

--- src/main/java/com/accp/action/lzh/logisticsAction.java
+++ src/main/java/com/accp/action/lzh/logisticsAction.java
@@ -1,11 +1,16 @@
 package com.accp.action.lzh;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.accp.biz.lzh.logisticsBiz;
@@ -53,6 +58,14 @@ public class logisticsAction {
 	public List<Sharea> getKoreaArea(@PathVariable String city){
 		return biz.getKoreaArea(city);
 	}
-	
-	
+	@RequestMapping(value="addLogis",method=RequestMethod.POST)
+	@ResponseBody
+	public Map<String,String> addLogistics(@RequestBody Logistics logis){
+		System.out.println(logis.getCollectgoods());
+		biz.addLogistics(logis);
+		Map<String, String> message = new HashMap<String, String>();
+		message.put("code", "200");
+		message.put("msg", "ok");
+		return message;
+	}
 }
